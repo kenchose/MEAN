5fb2fa92c02c5a53cdab64118fe12fc666313b57
Kenny Pham <kennympham@gmail.com>
2019-01-16 22:35:08 -0800

    Add files via upload

---
+++ Socket.io/Survey Form Revisited/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, '/static')))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+const server = app.listen(8000, () => console.log('Listening on port 8000'));
+const io = require('socket.io')(server);
+// let counter = 0;
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+io.on('connection', (socket) => {
+    socket.emit('greeting', {msg: 'Greetings, from server Node, brought to you by Sockets! -Server'})
+    socket.on('thankyou', (data) => {
+        console.log(data.msg);
+    })
+    socket.on('posting_form', (data) => { //socket.on listens for emitted information from the client and collects the data, socket.emit('form_submission')
+        //form information that was submited
+        console.log(`This information is coming from the submitted form: Name: ${data.name}, Location: ${data.location}, Language: ${data.language}, Comment: ${data.comment}`)
+        socket.emit('updated_message', {response: data})
+        // Second way to do it
+        // socket.emit('server_emit', {name: data.name, location: data.location, language: data.language, comment: data.comment})
+        socket.emit('random_number', {number: Math.floor(Math.random() * 1000 + 1)})
+    })
+})
+
